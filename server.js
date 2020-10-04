7ba6a49453b18ed1ef676c5cc643ec249ac6b4a4
Vlad Shynder <shynderv@gmail.com>
2020-10-04 20:32:52 +0300

    whatsapp clone server

---
+++ server.js
@@ -0,0 +1,20 @@
+const io = require("socket.io")(5000);
+
+io.on("connection", (socket) => {
+  const id = socket.handshake.query.id;
+  socket.join(id);
+
+  socket.on("send-message", ({ recipients, text }) => {
+    recipients.forEach((recipient) => {
+      const newRecipient = recipients.filter((r) => r !== recipient);
+      newRecipient.push(id);
+      socket.broadcast
+        .to(recipient)
+        .emit("recieve-message", {
+          recipients: newRecipient,
+          sender: id,
+          text,
+        });
+    });
+  });
+});
